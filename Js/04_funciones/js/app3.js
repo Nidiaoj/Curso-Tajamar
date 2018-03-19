2f0a4bcbd9c500eadb1801efb0c783f0e399d089
Nidiaoj <nidiave_oj@hotmail.com>
2018-03-19 21:30:42 +0100

    Nuevos Ejemplos funciones

---
+++ Js/04_funciones/js/app3.js
@@ -0,0 +1,27 @@
+function calcularCuadrado(a){
+    a *= a
+    return a 
+}
+function calcularCubo(a){
+    a = a * a * a
+    return a 
+}
+
+function mostarCalculo(n, f){
+    console.log(`El resulatado es ${f(n)}`)
+    return f(n)
+
+}
+let n = 5
+mostarCalculo(n, calcularCuadrado)
+console.log(mostrarCalculo(n, calcularCubo))
+console.log(mostrarCalculo(n, function(){
+    return n * 2
+}))
+setTimeout(function(){
+    console.log(calcularCubo(n))
+}, 3000)
+for (let i = 0; i < 10000; i++) {
+    console.log(i)
+    
+}
